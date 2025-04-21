f4a0f512b47006573aa19b311e1711919cefd3fc
Tavasya Ganpati <tavasya@sfs-wifi-dhcp-10-143-143-93.wifi.sfsu.edu>
2025-04-21 14:46:03 -0700

    <testing>: finished jest configuration

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }],
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+};
---
+++ src/tests/pages/StudentAssignmentDetailes.test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '@testing-library/jest-dom';
+
+
+
+describe('Basic Jest Setup', () => {
+    test("Jest is working", () => {
+        expect(true).toBe(true);
+    })
+})
